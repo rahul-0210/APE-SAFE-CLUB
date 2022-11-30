84bfa616707600f161ca35125f2a437bf57f94da
dhiren <dhiren.parmar@bombaysofware.com>
2022-11-30 07:46:53 +0530

    adding scroll to stacking page

--- src/components/layouts/Comingsoon.js
+++ src/components/layouts/Comingsoon.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import image from "../../assets/logo.png";
 const Comingsoon = ({title}) => (
     <div className="connectWallet">
         <div className="main">
             <div className="card-element">
                 <div className="card-content">
                     <div>
+                        <div className="container">
+                            <img
+                                alt="Launching Soon"
+                                className="projectImg"
+                                src={image}
+                                height={150}
+                                width={150}
+                            />
+                        </div>
                         <p className="">{title}</p>
                         <p>
                             <h3>Coming Soon</h3>
--- src/pages/dex.js
+++ src/pages/dex.js
@@ -1,14 +1,7 @@
-import {Box, Grid} from '@mui/material'
-import React from 'react'
-import PageHeader from '../components/PageHeader'
+import React from 'react';
+import Comingsoon from '../components/layouts/Comingsoon'
+
 
 export default function dex() {
-    return (
-        <Box sx={{p: 5}} className="dex">
-            <PageHeader title={`Dex`} />
-            <Box>
-                <Grid container spacing={3}></Grid>
-            </Box>
-        </Box>
-    )
+    return (<Comingsoon title="DEX" />)
 }
